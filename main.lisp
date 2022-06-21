478a2ecac568f175a8c97f90fb2b9f1ede59c903
Hectarea <hectometrocuadrado@gmail.com>
2022-06-22 00:16:47 +0200

    Update

--- main.lisp
+++ main.lisp
@@ -6,59 +6,62 @@
 (defconstant height 600)
 
 (defclass hello-triangle-application ()
-  ((window :accessor window :initform nil))
-  ((instance :accessor instance :initform nil)))
+  ((window :accessor window :initform nil)
+   (instance :accessor instance :initform nil)))
 
-(defmethod init-vulkan (obj)
-  (create-instance))
+(defun init-vulkan (app)
+  (create-instance app))
 
-(defmethod main-loop (obj)
-  (loop while (not (glfw:window-should-close (window obj)))
+(defun main-loop (app)
+  (loop while (not (glfw:window-should-close (window app)))
 	do (glfw:poll-events)))
 
-(defmethod cleanup (obj)
-  (glfw:destroy-window (window obj))
+(defun cleanup (app)
+  (cvk:destroy-instance (instance app) nil)
+  
+  (glfw:destroy-window (window app))
+  
   (glfw:terminate))
 
-(defmethod run (obj)
-  (init-window obj)
-  (init-vulkan obj)
-  (main-loop obj)
-  (cleanup obj))
+(defun run (app)
+  (init-window app)
+  (init-vulkan app)
+  (main-loop app)
+  (cleanup app))
 
-(defmethod init-window (obj)
+(defun init-window (app)
   (glfw:init)
   
   (glfw:window-hint glfw:GLFW_CLIENT_API glfw:GLFW_NO_API)
   (glfw:window-hint glfw:GLFW_RESIZABLE glfw:GLFW_FALSE)
 
-  (setf (window obj) (glfw:create-window width height "Vulkan" nil nil)))
+  (setf (window app) (glfw:create-window width height "Vulkan" nil nil)))
 
 
-(defun create-instance (obj)
-  (cvk:with-application-info app-info (:sType VK_STRUCTURE_TYPR_APPLICATION_INFO
-				       :pAppicationName "Hello triangle"
+(defun create-instance (app)
+  (cvk:with-application-info app-info (:sType cvk:VK_STRUCTURE_TYPE_APPLICATION_INFO
+				       :pApplicationName "Hello triangle"
 				       :applicationVersion (cvk:make-version 1 0 0)
 				       :pEngineName "No Engine"
 				       :engineVersion (cvk:make-version 1 0 0)
 				       :apiVersion (cvk:make-version 1 0 0))
     
-    (cvk:with-instance-create-info create-info (:sType VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
+    (cvk:with-instance-create-info create-info (:sType cvk:VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO
 						:pApplicationInfo app-info)
 
       (let ((glfw-extensions (glfw:get-required-instance-extensions)))
 
-	(setf (instance-create-info-enabledExtensionCount create-info) (length glfw-extensions))
-	(setf (instance-create-info-ppEnabledExtensionNames create-info) glfw-extensions)
+	(setf (cvk:instance-create-info-enabledExtensionCount create-info) (length glfw-extensions))
+	(setf (cvk:instance-create-info-ppEnabledExtensionNames create-info) glfw-extensions)
 
-	(setf (instance-create-info-enabledLayerCount create-info) 0)
+	(setf (cvk:instance-create-info-enabledLayerCount create-info) 0)
 
-	(multiple-value-bind (instance result) (cvk:create-instance create-info)
+	(multiple-value-bind (instance result) (cvk:create-instance create-info nil)
 	  
 	  (if (not (equal result cvk:VK_SUCCESS))
 	      (error "Failed to create instance"))
 
-	  (setf (instance obj) instance))))))
+	  (setf (instance app) instance))))))
 
 
 (defun main ()
